172bea415397024366437c0a8bfd3dd462fbba02
m-rauen <matheus.rauen@grad.ufsc.br>
2024-03-05 16:21:13 -0300

    Added functionality to mail option + created js links

---
+++ js/links.js
@@ -0,0 +1,10 @@
+export const links = [ 
+	{ 
+		name: 'GitHub', 
+		link: 'https://github.com/geem-lab', 
+	}, 
+	{ 
+		name: 'Zenodo', 
+		link: 'https://zenodo.org/communities/qcomp/records?q=&l=list&p=1&s=10&sort=newest', 
+	}, 
+];
