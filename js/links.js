898a9f20220307f108e0b4e15fca8f3f52b5b8af
m-rauen <matheus.rauen@grad.ufsc.br>
2024-03-12 11:32:12 -0300

    Added CV links + tree aligned and centralized

--- js/links.js
+++ js/links.js
@@ -7,4 +7,12 @@ export const links = [
 		name: 'Zenodo', 
 		link: 'https://zenodo.org/communities/qcomp/records?q=&l=list&p=1&s=10&sort=newest', 
 	}, 
+	{
+		name: 'Giovanni',
+		link: 'http://lattes.cnpq.br/0271249291940695',
+	},
+	{
+		name: 'Luis',
+		link: 'http://lattes.cnpq.br/1401417310558688'
+	}
 ];
