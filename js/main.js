daf60e662c4bd37878bb08a6d8266748d1a96e9d
m-rauen <matheus.rauen@grad.ufsc.br>
2024-03-19 10:22:28 -0300

    Cleaning code + preparing to deploy

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-function click_animation(chosenOption) {
-    const otherOptionId = chosenOption === 'link1' ? 'link2' :'link1';
-    const otherOption = document.getElementById(otherOptionId);
-    
-    otherOption.classList.add('hide');
-    
-    setTimeout(function() {
-    }, 1000); 
-}
-
 function scale_animation() {
     const option_btn = document.querySelector('.button');
     option_btn.classList.add('hover'); 
